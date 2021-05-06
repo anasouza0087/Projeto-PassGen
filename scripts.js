76151cc87269f5fc2a8779467709617b826d83fd
Ana <ana.souza0087@gmail.com>
2021-05-05 22:20:45 -0300

    criando eventos de verificação

--- scripts.js
+++ scripts.js
@@ -1,17 +1,31 @@
 const password = document.getElementById('passGen')
+const size6 = document.getElementById('6')
+const size8 = document.getElementById('8')
+const size10 = document.getElementById('10')
+const typeNum = document.getElementById('numeric')
+const typeAlfb = document.getElementById('alphabetic')
+const typeAlfn = document.getElementById('alpha-numeric')
 
-//Tamanho deverá ser dinamico - Tamanho da senha
-const digit = new Array(8)
+typeNum.addEventListener('click', e => {
+    const digit = new Array(6)
+    for (let i = 0; i < 6; i++) {
+        digit.push((Math.floor(Math.random() * 10)))
+        password.innerHTML = digit.join(' ')
+    }
+})
 
-//Função para senhas numéricas
-for (let i=0; i < 8; i++){
-    digit.push((Math.floor(Math.random() * 10)))
-    password.innerHTML = digit.join(' ')
-}
+typeAlfb.addEventListener('click', e => {
+    const digit = new Array(6)
+    for (let i = 0; i < 1; i++) {
+        digit.push(Math.random().toString(36).replace(/[^a-z]+/g, ''))
+        password.innerHTML = digit.join(' ')
+    }
+})
 
-//Função para senhas alfabéticas
-/*for (let i=0; i < 1; i++){
-    digit.push(Math.random().toString(36).replace(/[^a-z]+/g, ''))
-    password.innerHTML = digit.join(' ')
-}*/
-
+typeAlfn.addEventListener('click', e => {
+    const digit = new Array(6)
+    for (let i = 0; i < 1; i++) {
+        digit.push(Math.random().toString(36).replace(/[^A-Za-z0-9]+/g, ''))
+        password.innerHTML = digit.join(' ')
+    }
+})
