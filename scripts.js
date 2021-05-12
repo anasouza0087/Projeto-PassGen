3e55966da6ee2282cd573301f85ccb4922144332
Ana <ana.souza0087@gmail.com>
2021-05-11 21:34:20 -0300

    Concluíndo funções para criação da senha

--- scripts.js
+++ scripts.js
@@ -5,27 +5,104 @@ const size10 = document.getElementById('10')
 const typeNum = document.getElementById('numeric')
 const typeAlfb = document.getElementById('alphabetic')
 const typeAlfn = document.getElementById('alpha-numeric')
+const btnGerarSenha = document.getElementById('gerarSenha')
 
-typeNum.addEventListener('click', e => {
-    const digit = new Array(6)
-    for (let i = 0; i < 6; i++) {
-        digit.push((Math.floor(Math.random() * 10)))
-        password.innerHTML = digit.join(' ')
-    }
+//Senha tamanho 6
+size6.addEventListener('click', e => {
+    typeNum.addEventListener('click', e => {
+        const digit = new Array(6)
+        for (let i = 0; i < 6; i++) {
+            digit.push((Math.floor(Math.random() * 10)))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
+
+    typeAlfb.addEventListener('click', e => {
+        const digit = new Array(6)
+        for (let i = 0; i < 1; i++) {
+            digit.push(Math.random().toString(36).replace(/[^a-z]+/g, ''))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
+
+    typeAlfn.addEventListener('click', e => {
+        const digit = new Array(6)
+        for (let i = 0; i < 1; i++) {
+            digit.push(Math.random().toString(36).replace(/[^A-Za-z0-9]+/g, ''))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
 })
 
-typeAlfb.addEventListener('click', e => {
-    const digit = new Array(6)
-    for (let i = 0; i < 1; i++) {
-        digit.push(Math.random().toString(36).replace(/[^a-z]+/g, ''))
-        password.innerHTML = digit.join(' ')
-    }
+//Senha tamanho 8
+size8.addEventListener('click', e => {
+    typeNum.addEventListener('click', e => {
+        const digit = new Array(8)
+        for (let i = 0; i < 8; i++) {
+            digit.push((Math.floor(Math.random() * 10)))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
+
+    typeAlfb.addEventListener('click', e => {
+        const digit = new Array(8)
+        for (let i = 0; i < 1; i++) {
+            digit.push(Math.random().toString(36).replace(/[^a-z]+/g, ''))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
+
+    typeAlfn.addEventListener('click', e => {
+        const digit = new Array(8)
+        for (let i = 0; i < 1; i++) {
+            digit.push(Math.random().toString(36).replace(/[^A-Za-z0-9]+/g, ''))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
 })
 
-typeAlfn.addEventListener('click', e => {
-    const digit = new Array(6)
-    for (let i = 0; i < 1; i++) {
-        digit.push(Math.random().toString(36).replace(/[^A-Za-z0-9]+/g, ''))
-        password.innerHTML = digit.join(' ')
-    }
+//Senha tamanho 10
+size10.addEventListener('click', e => {
+    typeNum.addEventListener('click', e => {
+        const digit = new Array(10)
+        for (let i = 0; i < 10; i++) {
+            digit.push((Math.floor(Math.random() * 10)))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
+
+    typeAlfb.addEventListener('click', e => {
+        const digit = new Array(10)
+        for (let i = 0; i < 1; i++) {
+            digit.push(Math.random().toString(36).replace(/[^a-z]+/g, ''))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
+
+    typeAlfn.addEventListener('click', e => {
+        const digit = new Array(10)
+        for (let i = 0; i < 1; i++) {
+            digit.push(Math.random().toString(36).replace(/[^A-Za-z0-9]+/g, ''))
+            btnGerarSenha.addEventListener('click', e => {
+                password.innerHTML = digit.join(' ')
+            })
+        }
+    })
 })
+
